432a3197e4242e214f392ff5d9be0bde16220054
CodeCin523 <155215757+CodeCin523@users.noreply.github.com>
2026-05-14 16:15:42 -0400

    it's working

--- apps/server/config/db.js
+++ apps/server/config/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
+const password = encodeURIComponent(process.env.DB_PASSWORD);
+const uri = `mongodb+srv://${process.env.DB_USER}:${password}@cluster0.u5ore9c.mongodb.net/?appName="Cluster0"`;
+
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(uri, {serverApi:{version:'1',strict:true,deprecationErrors:true}});
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
